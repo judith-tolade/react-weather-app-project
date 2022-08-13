fd03ad3daffb53381abfb4212370be42c81fe77c
judith-tolade <101555581+judith-tolade@users.noreply.github.com>
2022-08-13 01:32:25 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-          Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Judith-Fashipe and is {""}
+        <a
+          href="https://github.com/judith-tolade/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
